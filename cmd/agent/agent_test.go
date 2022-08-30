0c291de0614114df2266517e9a45b96f34b5d222
Raphael Reyna <raphaelreyna@protonmail.com>
2022-08-30 14:27:04 -0700

    create test suite with cleanup
    Signed-off-by: Raphael Reyna <raphaelreyna@protonmail.com>

--- cmd/agent/agent_test.go
+++ cmd/agent/agent_test.go
@@ -2,35 +2,150 @@ package agent_test
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	apiv1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/datawire/dlib/dexec"
 	"github.com/datawire/dlib/dlog"
 
 	"github.com/emissary-ingress/emissary/v3/pkg/dtest"
-	"github.com/emissary-ingress/emissary/v3/pkg/k8s"
 	"github.com/emissary-ingress/emissary/v3/pkg/kates"
-	"github.com/emissary-ingress/emissary/v3/pkg/kubeapply"
 )
 
-func TestStandalone_StayAlive(t *testing.T) {
-	ctx := dlog.NewTestContext(t, false)
-	kubeconfig := dtest.KubeVersionConfig(ctx, dtest.Kube22)
-	cli, err := kates.NewClient(kates.ClientConfig{Kubeconfig: kubeconfig})
-	require.NoError(t, err)
+type BasicTestSuite struct {
+	suite.Suite
 
-	setup(t, ctx, kubeconfig, cli)
+	ctx context.Context
 
+	cli       *kates.Client
+	namespace string
+	name      string
+
+	resources []any
+}
+
+func TestBasicTestSuite(t *testing.T) {
+	suite.Run(t, &BasicTestSuite{})
+}
+
+func (s *BasicTestSuite) SetupTest() {
+	s.namespace = "ambassador-test"
+	s.name = "ambassador-agent"
+	s.ctx = dlog.NewTestContext(s.T(), false)
+
+	kubeconfig := dtest.KubeVersionConfig(s.ctx, dtest.Kube22)
+
+	var err error
+	s.cli, err = kates.NewClient(kates.ClientConfig{Kubeconfig: kubeconfig})
+	s.NoError(err)
+
+	// This env var is used by ./testdata/agent.yaml
+	s.NotEmpty(os.Getenv("AMBASSADOR_AGENT_DOCKER_IMAGE"))
+
+	s.resources = []any{
+		apiv1.Namespace{
+			TypeMeta: metav1.TypeMeta{
+				Kind: "Namespace",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: s.namespace,
+			},
+		},
+		rbacv1.ClusterRole{
+			TypeMeta: metav1.TypeMeta{
+				Kind: "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: s.name,
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{""},
+					Resources: []string{
+						"namespaces",
+						"services",
+						"secrets",
+						"configmaps",
+						"pods",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+					},
+				},
+			},
+		},
+		rbacv1.ClusterRoleBinding{
+			TypeMeta: metav1.TypeMeta{
+				Kind: "ClusterRoleBinding",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: s.name,
+			},
+			RoleRef: rbacv1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "ClusterRole",
+				Name:     s.name,
+			},
+			Subjects: []rbacv1.Subject{
+				{
+					Kind:      "ServiceAccount",
+					Name:      s.name,
+					Namespace: s.namespace,
+				},
+			},
+		},
+		apiv1.ServiceAccount{
+			TypeMeta: metav1.TypeMeta{
+				Kind: "ServiceAccount",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      s.name,
+				Namespace: s.namespace,
+			},
+		},
+		apiv1.Pod{
+			TypeMeta: metav1.TypeMeta{
+				Kind: "Pod",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      s.name,
+				Namespace: s.namespace,
+			},
+			Spec: apiv1.PodSpec{
+				ServiceAccountName: s.name,
+				Containers: []apiv1.Container{
+					{
+						Image: os.Getenv("AMBASSADOR_AGENT_DOCKER_IMAGE"),
+						Name:  s.name,
+					},
+				},
+			},
+		},
+	}
+
+	for _, resource := range s.resources {
+		err := s.cli.Create(s.ctx, resource, nil)
+		s.Require().NoError(err)
+	}
+
+	time.Sleep(3 * time.Second)
+}
+
+func (s *BasicTestSuite) TearDownSuite() {
+	ctx := context.Background()
+	for i := len(s.resources) - 1; 0 <= i; i-- {
+		s.cli.Delete(ctx, s.resources[i], nil)
+	}
+}
+
+func (s *BasicTestSuite) TestStandalone_StayAlive() {
 	// lets make sure the agent came up and stays up
 	time.Sleep(time.Second * 10)
 
@@ -39,68 +154,15 @@ func TestStandalone_StayAlive(t *testing.T) {
 			Kind: "Pod",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "ambassador-agent",
-			Namespace: "ambassador",
+			Name:      s.name,
+			Namespace: s.namespace,
 		},
 	}
-	err = cli.Get(ctx, agentPod, &agentPod)
-	require.NoError(t, err)
-	require.NotEmpty(t, agentPod.Labels)
-}
 
-func needsDockerBuilds(ctx context.Context, var2file map[string]string) error {
-	// TODO(lukeshu): Consider unifying envoytest.GetLocalEnvoyImage() with
-	// agent_test.go:needsDockerBuilds().
-	var targets []string
-	for varname, filename := range var2file {
-		if os.Getenv(varname) == "" {
-			targets = append(targets, filename)
-		}
-	}
-	if len(targets) == 0 {
-		return nil
-	}
-	if os.Getenv("DEV_REGISTRY") == "" {
-		registry := dtest.DockerRegistry(ctx)
-		os.Setenv("DEV_REGISTRY", registry)
-		os.Setenv("DTEST_REGISTRY", registry)
-	}
-	cmdline := append([]string{"make", "-C", "../.."}, targets...)
-	if err := dexec.CommandContext(ctx, cmdline[0], cmdline[1:]...).Run(); err != nil {
-		return err
-	}
-	for varname, filename := range var2file {
-		if os.Getenv(varname) == "" {
-			dat, err := ioutil.ReadFile(filepath.Join("../..", filename))
-			if err != nil {
-				return err
-			}
-			lines := strings.Split(strings.TrimSpace(string(dat)), "\n")
-			if len(lines) < 2 {
-				return fmt.Errorf("malformed docker.mk tagfile %q", filename)
-			}
-			if err := os.Setenv(varname, lines[1]); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
+	err := s.cli.Get(s.ctx, agentPod, &agentPod)
+	s.Require().NoError(err)
+	s.NotEmpty(agentPod.Status.ContainerStatuses)
+	s.True(agentPod.Status.ContainerStatuses[0].Ready)
 
-func setup(t *testing.T, ctx context.Context, kubeconfig string, cli *kates.Client) {
-	require.NoError(t, needsDockerBuilds(ctx, map[string]string{
-		"AMBASSADOR_AGENT_DOCKER_IMAGE": "docker/emissary.docker.push.remote",
-		"KAT_SERVER_DOCKER_IMAGE":       "docker/kat-server.docker.push.remote",
-	}))
-
-	image := os.Getenv("AMBASSADOR_AGENT_DOCKER_IMAGE")
-	require.NotEmpty(t, image)
-
-	kubeinfo := k8s.NewKubeInfo(kubeconfig, "", "")
-
-	require.NoError(t, kubeapply.Kubeapply(ctx, kubeinfo, time.Minute, true, false, "./testdata/namespace.yaml"))
-	require.NoError(t, kubeapply.Kubeapply(ctx, kubeinfo, 2*time.Minute, true, false, "./testdata/agent.yaml"))
-	require.NoError(t, kubeapply.Kubeapply(ctx, kubeinfo, 2*time.Minute, true, false, "./testdata/fake-agentcom.yaml"))
-
-	time.Sleep(3 * time.Second)
+	s.NoError(err)
 }
