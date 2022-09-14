4379c792c83cc0aeaad5e3e5fea54173513d1f8b
njayp <nickjaypowell@gmail.com>
2022-09-14 02:53:11 -0700

    fix clients
    Signed-off-by: njayp <nickjaypowell@gmail.com>

--- pkg/agent/watchers.go
+++ pkg/agent/watchers.go
@@ -18,7 +18,7 @@ type Watchers struct {
 	endpointWatcher *k8sapi.Watcher[*kates.Endpoints]
 }
 
-func NewWatchers(client rest.Interface) *Watchers {
+func NewWatchers(appClient rest.Interface, coreClient rest.Interface) *Watchers {
 	// This function compares the recieved object with the cached object
 	// and decides whether an update should be pushed
 	compareFunc := func(o1, o2 runtime.Object) bool {
@@ -27,16 +27,16 @@ func NewWatchers(client rest.Interface) *Watchers {
 	}
 
 	cond := &sync.Cond{
-		L: &sync.RWMutex{},
+		L: &sync.Mutex{},
 	}
 
-	watchedNs := ""
+	watchedNs := "" // empty string watches all ns
 
 	return &Watchers{
-		mapsWatcher:     k8sapi.NewWatcher("configmap", watchedNs, client, &kates.ConfigMap{}, cond, compareFunc),
-		deployWatcher:   k8sapi.NewWatcher("deploy", watchedNs, client, &kates.Deployment{}, cond, compareFunc),
-		podWatcher:      k8sapi.NewWatcher("pod", watchedNs, client, &kates.Pod{}, cond, compareFunc),
-		endpointWatcher: k8sapi.NewWatcher("endpoints", watchedNs, client, &kates.Endpoints{}, cond, compareFunc),
+		mapsWatcher:     k8sapi.NewWatcher("configmaps", watchedNs, coreClient, &kates.ConfigMap{}, cond, compareFunc),
+		deployWatcher:   k8sapi.NewWatcher("deployments", watchedNs, appClient, &kates.Deployment{}, cond, compareFunc),
+		podWatcher:      k8sapi.NewWatcher("pods", watchedNs, coreClient, &kates.Pod{}, cond, compareFunc),
+		endpointWatcher: k8sapi.NewWatcher("endpoints", watchedNs, coreClient, &kates.Endpoints{}, cond, compareFunc),
 		cond:            cond,
 	}
 }
@@ -48,13 +48,13 @@ func (w *Watchers) EnsureStarted(ctx context.Context) {
 	w.endpointWatcher.EnsureStarted(ctx, nil)
 }
 
-type APIWatchers struct {
+type ConfigWatchers struct {
 	cond          *sync.Cond
 	mapsWatcher   *k8sapi.Watcher[*kates.ConfigMap]
 	secretWatcher *k8sapi.Watcher[*kates.Secret]
 }
 
-func NewAPIWatchers(client rest.Interface) *APIWatchers {
+func NewConfigWatchers(client rest.Interface, watchedNs string) *ConfigWatchers {
 	// This function compares the recieved object with the cached object
 	// and decides whether an update should be pushed
 	compareFunc := func(o1, o2 runtime.Object) bool {
@@ -63,19 +63,17 @@ func NewAPIWatchers(client rest.Interface) *APIWatchers {
 	}
 
 	cond := &sync.Cond{
-		L: &sync.RWMutex{},
+		L: &sync.Mutex{},
 	}
 
-	watchedNs := ""
-
-	return &APIWatchers{
-		mapsWatcher:   k8sapi.NewWatcher("configmap", watchedNs, client, &kates.ConfigMap{}, cond, compareFunc),
-		secretWatcher: k8sapi.NewWatcher("secret", watchedNs, client, &kates.Secret{}, cond, compareFunc),
+	return &ConfigWatchers{
+		mapsWatcher:   k8sapi.NewWatcher("configmaps", watchedNs, client, &kates.ConfigMap{}, cond, compareFunc),
+		secretWatcher: k8sapi.NewWatcher("secrets", watchedNs, client, &kates.Secret{}, cond, compareFunc),
 		cond:          cond,
 	}
 }
 
-func (w *APIWatchers) EnsureStarted(ctx context.Context) {
+func (w *ConfigWatchers) EnsureStarted(ctx context.Context) {
 	w.mapsWatcher.EnsureStarted(ctx, nil)
 	w.secretWatcher.EnsureStarted(ctx, nil)
 }
