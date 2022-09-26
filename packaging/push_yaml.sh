b5354351a8e35e3e438a25a133c827b9f3dea2d8
Jose Cortes <josecortes@datawire.io>
2022-09-26 13:09:27 -0400

    Build and push yaml

--- packaging/push_yaml.sh
+++ packaging/push_yaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 # This will be running in automation so we don't want the
 # interactive pager to pop up
@@ -14,15 +14,21 @@ echo "$TOP_DIR"
 tmpdir=$(mktemp -d)
 
 helm package \
-    --version=$A8R_AGENT_VERSION \
-    --app-version=$A8R_AGENT_VERSION \
-    --destination $tmpdir \
-    $TOP_DIR/helm/ambassador-agent
+    --version="$A8R_AGENT_VERSION" \
+    --app-version="$A8R_AGENT_VERSION" \
+    --destination "$tmpdir" \
+    "$TOP_DIR/helm/ambassador-agent"
 package_files=("$tmpdir"/ambassador-agent-*.tgz)
 package_file=${package_files[0]}
 
+yaml_file="${tmpdir}/ambassador-agent.yaml"
+helm template --skip-tests ambassador-agent "${package_file}" > "$yaml_file"
+
+
+
 bucket=${AWS_BUCKET:-datawire-static-files}
-prefix=${BUCKET_DIR:-charts-dev}
+prefix="${BUCKET_DIR:-yaml}/ambassador-agent/${A8R_AGENT_VERSION}"
+
 
 if [[ -z "$AWS_ACCESS_KEY_ID" ]] ; then
     echo "AWS_ACCESS_KEY_ID is not set"
@@ -32,15 +38,16 @@ elif [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
     exit 1
 fi
 
-echo "Checking that chart hasn't already been pushed by looking in ${bucket} / ${prefix}/${package_file##*/}"
+
+echo "Checking that yaml hasn't already been pushed by looking in ${bucket} / ${prefix}/${yaml_file##*/}"
 # We don't need the whole object, we just need the metadata
 # to see if it exists or not, so this is better than requesting
 # the whole tar file.
 if aws s3api head-object \
     --bucket "$bucket" \
-    --key "${prefix}/${package_file##*/}"
+    --key "${prefix}/${yaml_file##*/}"
 then
-    echo "Chart ${prefix}/${package_file##*/} has already been pushed."
+    echo "Chart ${prefix}/${yaml_file##*/} has already been pushed."
     exit 1
 
 fi
@@ -49,12 +56,20 @@ fi
 # S3 side that will re-generate the helm chart index when new objects are
 # added.
 echo "Pushing chart to S3 bucket $bucket"
-echo "Pushing ${prefix}/${package_file##*/}"
+echo "Pushing ${prefix}/${yaml_file##*/}"
 aws s3api put-object \
     --bucket "$bucket" \
-    --key "${prefix}/${package_file##*/}" \
-    --body "$package_file"
-echo "Successfully pushed ${prefix}/${package_file##*/}"
+    --key "${prefix}/${yaml_file##*/}" \
+    --body "$yaml_file"
+echo "Successfully pushed ${prefix}/${yaml_file##*/}"
+
+if [ "$A8R_AGENT_VERSION" != "*-*" ]; then
+    echo "${A8R_AGENT_VERSION}" "$tmpdir/stable.txt"
+    aws s3api put-object \
+        --bucket "$bucket" \
+        --key "$BUCKET_DIR/ambassador-agent/stable.txt" \
+        --body "$tmpdir/stable.txt"
+fi
 
 # Clean up
 rm -rf "$tmpdir"
