52d0e5d64bc60638924b9aea3ba91f575cb65a75
Raphael Reyna <raphaelreyna@protonmail.com>
2022-09-29 19:29:37 -0700

    clean up rbac test

--- integration_tests/basic_test/rbac_test.go
+++ integration_tests/basic_test/rbac_test.go
@@ -1,6 +1,8 @@
 package basic_test
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 func (s *BasicTestSuite) TestRBAC() {
 	roles, err := s.clientset.RbacV1().Roles("default").List(s.ctx, metav1.ListOptions{
@@ -11,13 +13,13 @@ func (s *BasicTestSuite) TestRBAC() {
 	cr, err := s.clientset.RbacV1().ClusterRoles().Get(s.ctx, s.name, metav1.GetOptions{})
 	if 0 < len(s.namespaces) {
 		s.Require().Error(err)
-		s.Require().Contains(err.Error(), "not found")
+		s.Contains(err.Error(), "not found")
 
-		s.Require().Less(1, len(roles.Items))
+		s.Less(1, len(roles.Items))
 	} else {
 		s.Require().NoError(err)
-		s.Require().NotNil(cr)
+		s.NotNil(cr)
 
-		s.Require().Equal(0, len(roles.Items))
+		s.Equal(0, len(roles.Items))
 	}
 }
