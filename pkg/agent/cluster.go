abe57211532dec38ad1290e6af749f4ebcc01e0b
Raphael Reyna <raphaelreyna@protonmail.com>
2022-10-17 15:39:02 -0700

    include ambassador id
    Signed-off-by: Raphael Reyna <raphaelreyna@protonmail.com>

--- pkg/agent/cluster.go
+++ pkg/agent/cluster.go
@@ -3,6 +3,7 @@ package agent
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/datawire/dlib/dlog"
@@ -11,29 +12,37 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-func GetClusterID(ctx context.Context, client *kubernetes.Clientset, nsName string) (clusterID string) {
-	clusterID = getEnvWithDefault("AMBASSADOR_CLUSTER_ID", getEnvWithDefault("AMBASSADOR_SCOUT_ID", ""))
+func GetClusterID(ctx context.Context, client *kubernetes.Clientset, nsName string) string {
+	clusterID := getEnvWithDefault("AMBASSADOR_CLUSTER_ID", getEnvWithDefault("AMBASSADOR_SCOUT_ID", ""))
 	if clusterID != "" {
 		return clusterID
 	}
 
 	dlog.Infof(ctx, "Fetching cluster ID from %s namespace", nsName)
 
+	rootID := "00000000-0000-0000-0000-000000000000"
+
 	ns, err := client.CoreV1().Namespaces().Get(ctx, nsName, v1.GetOptions{})
 	if err == nil {
-		clusterID = string(ns.GetUID())
-	} else {
-		dlog.Errorf(ctx, "Unable to detect cluster ID: %v", err)
-		return ""
+		rootID = string(ns.GetUID())
 	}
 
-	dlog.Infof(ctx, "Cluster ID is %s", clusterID)
+	dlog.Infof(ctx, "Using root ID %s", rootID)
 	dlog.Debugf(ctx, "Namespace looks like %+v", ns)
 	return clusterIDFromRootID(clusterID)
 }
 
 func clusterIDFromRootID(rootID string) string {
-	clusterUrl := fmt.Sprintf("d6e_id://%s/00000000-0000-0000-0000-000000000000", rootID)
+	clusterUrl := fmt.Sprintf("d6e_id://%s/%s", rootID, getAmbassadorID())
 	uid := uuid.NewSHA1(uuid.NameSpaceURL, []byte(clusterUrl))
+
 	return strings.ToLower(uid.String())
+}
+
+func getAmbassadorID() string {
+	id := os.Getenv("AMBASSADOR_ID")
+	if id == "" {
+		id = "default"
+	}
+	return id
 }
