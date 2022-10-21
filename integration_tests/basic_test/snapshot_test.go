c2fc1a9afdce06a9fa16207859a9508ba0834bac
Raphael Reyna <raphaelreyna@protonmail.com>
2022-10-21 13:17:14 -0700

    add snapshot test
    Signed-off-by: Raphael Reyna <raphaelreyna@protonmail.com>

--- integration_tests/basic_test/snapshot_test.go
+++ integration_tests/basic_test/snapshot_test.go
@@ -9,7 +9,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
-func (s *BasicTestSuite) TestEmptySnapshot() {
+func (s *BasicTestSuite) TestInitialSnapshot() {
 	ctx, cancel := context.WithTimeout(s.ctx, 5*time.Second)
 	defer cancel()
 	ss, err := s.agentComServer.GetSnapshot(ctx)
@@ -20,4 +20,51 @@ func (s *BasicTestSuite) TestEmptySnapshot() {
 	s.Require().NoError(err)
 
 	s.Empty(snapshot.Deltas)
+}
+
+func (s *BasicTestSuite) TestSnapshot() {
+	ctx, cancel := context.WithTimeout(s.ctx, 15*time.Second)
+	defer cancel()
+	ss, err := s.agentComServer.GetSnapshot(ctx)
+	s.Require().NoError(err)
+
+	var snapshot snapshotTypes.Snapshot
+	err = json.Unmarshal(ss.RawSnapshot, &snapshot)
+	s.Require().NoError(err)
+
+	for _, pod := range snapshot.Kubernetes.Pods {
+		s.Equal(pod.APIVersion, "v1")
+		s.Equal(pod.Kind, "Pod")
+		s.Empty(pod.ObjectMeta.ManagedFields)
+	}
+
+	for _, dep := range snapshot.Kubernetes.Endpoints {
+		s.Equal(dep.APIVersion, "v1")
+		s.Equal(dep.Kind, "Endpoint")
+		s.Empty(dep.ObjectMeta.ManagedFields)
+	}
+
+	for _, svc := range snapshot.Kubernetes.Services {
+		s.Equal(svc.APIVersion, "v1")
+		s.Equal(svc.Kind, "Service")
+		s.Empty(svc.ObjectMeta.ManagedFields)
+	}
+
+	for _, svc := range snapshot.Kubernetes.ConfigMaps {
+		s.Equal(svc.APIVersion, "v1")
+		s.Equal(svc.Kind, "ConfigMap")
+		s.Empty(svc.ObjectMeta.ManagedFields)
+	}
+
+	for _, svc := range snapshot.Kubernetes.Ingresses {
+		s.Equal(svc.APIVersion, "networking.kubernetes.io/v1")
+		s.Equal(svc.Kind, "Ingress")
+		s.Empty(svc.ObjectMeta.ManagedFields)
+	}
+
+	for _, dep := range snapshot.Kubernetes.Deployments {
+		s.Equal(dep.APIVersion, "apps/v1")
+		s.Equal(dep.Kind, "Deployment")
+		s.Empty(dep.ObjectMeta.ManagedFields)
+	}
 }
