5a5e2ca3b8f6e180967091f2de9b32522b3ceafe
Thomas Hallgren <thomas@datawire.io>
2022-12-30 14:00:02 +0100

    Add proper cleanup functionality to the integration tests.
    And move some repeated boilerplate initialization code into a reusable
    struct.
    
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- integration_tests/aes_test/agentInstallation_test.go
+++ integration_tests/aes_test/agentInstallation_test.go
@@ -5,48 +5,40 @@ import (
 	"strings"
 	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	itest "github.com/datawire/ambassador-agent/integration_tests"
+	"github.com/datawire/dlib/dlog"
 )
 
 func (s *AESTestSuite) TestAgentInstallation() {
 	installationConfig := itest.InstallationConfig{
-		ReleaseName: s.name,
-		Namespace:   s.namespace,
+		ReleaseName: s.Name(),
+		Namespace:   s.Namespace(),
 		ChartDir:    "../../helm/ambassador-agent",
 		Values: map[string]any{
 			"rpcAddress": s.agentComServer.RPCAddress(),
 		},
 
-		RESTConfig: s.config,
+		RESTConfig: s.Config(),
 		Log:        s.T().Logf,
 	}
-	uninstallHelmChart, err := itest.InstallHelmChart(s.ctx, installationConfig)
+	ctx := s.Context()
+	uninstallHelmChart, err := itest.InstallHelmChart(ctx, installationConfig)
 	s.Require().NoError(err)
-	s.cleanupFuncs["agent helm chart"] = uninstallHelmChart
+	s.Cleanup(uninstallHelmChart)
 
-	time.Sleep(5 * time.Second)
-
-	pods, err := s.clientset.CoreV1().Pods(s.namespace).List(s.ctx, metav1.ListOptions{
-		LabelSelector: "app.kubernetes.io/name=ambassador-agent",
-	})
-	s.Require().NoError(err)
-	s.Require().NotEmpty(pods.Items)
-	agentPod := pods.Items[0]
-
-	ctx, cancel := context.WithTimeout(s.ctx, 30*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
 	defer cancel()
 
-	logLines, err := itest.NewPodLogChan(ctx, s.clientset, agentPod.Name, s.namespace, true)
+	logLines, err := itest.NewPodLogChan(ctx, s.K8sIf(), itest.AgentLabelSelector, s.Namespace(), true)
 	s.Require().NoError(err)
 
 	var succ bool
 	for line := range logLines {
-		if succ = strings.Contains(line, "Setting cloud connect token"); succ {
+		dlog.Info(ctx, line)
+		if succ = strings.Contains(line, "Setting cloud connect token from environment"); succ {
+			cancel()
 			break
 		}
 	}
-
 	s.True(succ)
 }
---
+++ integration_tests/suite.go
@@ -0,0 +1,98 @@
+package itest
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/datawire/dlib/dlog"
+	"github.com/datawire/dlib/dtime"
+)
+
+type Suite struct {
+	suite.Suite
+	ctx       context.Context
+	config    *rest.Config
+	k8sIf     kubernetes.Interface
+	namespace string
+	name      string
+}
+
+func (s *Suite) K8sIf() kubernetes.Interface {
+	return s.k8sIf
+}
+
+func (s *Suite) Context() context.Context {
+	return s.ctx
+}
+
+func (s *Suite) Config() *rest.Config {
+	return s.config
+}
+
+func (s *Suite) Name() string {
+	return s.name
+}
+
+func (s *Suite) Namespace() string {
+	return s.namespace
+}
+
+func (s *Suite) Init() {
+	s.namespace = "ambassador-test"
+	s.name = "ambassador-agent"
+	s.ctx = dlog.NewTestContext(s.T(), false)
+	kubeconfigPath := os.Getenv("KUBECONFIG")
+	s.Require().NotEmpty(kubeconfigPath, "%s needs to be set", "KUBECONFIG")
+
+	var err error
+	s.config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	s.Require().NoError(err)
+	s.k8sIf, err = kubernetes.NewForConfig(s.config)
+	s.Require().NoError(err)
+}
+
+func (s *Suite) Cleanup(f func(context.Context) error) {
+	s.T().Cleanup(func() {
+		s.NoError(f(s.ctx))
+	})
+}
+
+func (s *Suite) CreateNamespace(ctx context.Context, ns string) error {
+	_, err := s.k8sIf.CoreV1().Namespaces().Create(ctx,
+		&corev1.Namespace{
+			ObjectMeta: v1.ObjectMeta{
+				Name: ns,
+			},
+		},
+		v1.CreateOptions{},
+	)
+	return err
+}
+
+func (s *Suite) DeleteNamespace(ctx context.Context, ns string) error {
+	nsAPI := s.k8sIf.CoreV1().Namespaces()
+	if err := nsAPI.Delete(ctx, ns, v1.DeleteOptions{}); err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(ctx, time.Minute)
+	defer cancel()
+	for {
+		_, err := nsAPI.Get(ctx, ns, v1.GetOptions{})
+		if err != nil {
+			if errors.IsNotFound(err) {
+				err = nil
+			}
+			return err
+		}
+		dtime.SleepWithContext(ctx, 5*time.Second)
+	}
+}
