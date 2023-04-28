aa66672af75d060dca1cd3e5ac0e734be437562b
jacoblbeck <jacobbeck2112@gmail.com>
2023-04-28 11:34:24 -0500

    added new integration tests for cloud connect tokens

--- integration_tests/cloudToken_test/cloudtoken_test.go
+++ integration_tests/cloudToken_test/cloudtoken_test.go
@@ -11,7 +11,7 @@ import (
 	itest "github.com/datawire/ambassador-agent/integration_tests"
 )
 
-func (s *CloudTokenTestSuite) TestCloudToken() {
+func (s *CloudTokenTestSuite) TestCloudTokenWithSecret() {
 	secret := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      s.Name() + "-cloud-token",
@@ -38,6 +38,59 @@ func (s *CloudTokenTestSuite) TestCloudToken() {
 	var succ bool
 	for line := range logLines {
 		if succ = strings.Contains(line, "Setting cloud connect token from secret"); succ {
+			cancel()
+			break
+		}
+	}
+	s.True(succ)
+}
+
+func (s *CloudTokenTestSuite) TestCloudTokenWithConfigMap() {
+	cm := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      s.Name() + "-cloud-token",
+			Namespace: s.Namespace(),
+		},
+		Data: map[string]string{
+			"CLOUD_CONNECT_TOKEN": "TEST_TOKEN",
+		},
+	}
+
+	ctx := s.Context()
+	scAPI := s.K8sIf().CoreV1().ConfigMaps(s.Namespace())
+	_, err := scAPI.Create(ctx, &cm, metav1.CreateOptions{})
+	s.Require().NoError(err)
+	s.Cleanup(func(ctx context.Context) error {
+		return scAPI.Delete(ctx, cm.ObjectMeta.Name, metav1.DeleteOptions{})
+	})
+
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	defer cancel()
+
+	logLines, err := itest.NewPodLogChan(ctx, s.K8sIf(), itest.AgentLabelSelector, s.Namespace(), true)
+	s.Require().NoError(err)
+
+	var succ bool
+	for line := range logLines {
+		if succ = strings.Contains(line, "Setting cloud connect token from configmap"); succ {
+			cancel()
+			break
+		}
+	}
+	s.True(succ)
+}
+
+func (s *CloudTokenTestSuite) TestNoCloudToken() {
+	ctx := s.Context()
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	defer cancel()
+
+	logLines, err := itest.NewPodLogChan(ctx, s.K8sIf(), itest.AgentLabelSelector, s.Namespace(), true)
+	s.Require().NoError(err)
+
+	var succ bool
+	for line := range logLines {
+		if succ = strings.Contains(line, "Unable to get cloud connect token. This agent will do nothing."); succ {
 			cancel()
 			break
 		}
